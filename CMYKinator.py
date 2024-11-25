e95d7bac33c8f680e3fcad1aff404fdcdf053502
Kathryn Schwochert <kes210001@utdallas.edu>
2024-11-25 12:33:17 -0600

    pseudocoding

---
+++ CMYKinator.py
@@ -0,0 +1,50 @@
+import PIL
+
+def main():
+# main function that runs the program
+
+
+def rgb_to_cmyk():
+# function converts rgb image to jpeg
+# function converts rgb image to cmyk
+
+
+def watermark():
+# function overlays one image on top of the cmyk image
+# function includes a transparency option to adjust the overlaid image
+# function resizes overlaid image
+# function includes placement option 
+
+
+def adjust_cyan():
+# function breaks apart cmyk channels and adjusts the cyan channel
+
+
+def adjust_magenta():
+# function breaks apart cmyk channels and adjusts the magenta channel
+
+
+def adjust_yellow():
+# function breaks apart cmyk channels and adjusts the yellow channel
+
+
+def adjust_black():
+# function breaks apart cmyk channels and adjusts the black/k channel
+
+
+def resize_img():
+# function resizes image
+
+
+def adjust_contrast():
+# function adjusts contrast of image
+
+
+def batch_render():
+# function batch renders images with similar file names such as "artworkRGB.jpg and drawingRGB.jpg"
+
+
+
+
+if __name__ == "__main__":
+    main()
